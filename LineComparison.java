3c76a608b8b087fe93d8805bd05a9ec8546cf14e
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-02 23:35:58 +0530

    UC1

--- LineComparison.java
+++ LineComparison.java
@@ -1,9 +1,29 @@
 package com.day8LineCom;
 
 public class LineComparison {
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	
+	LineComparison(int x1, int x2, int y1, int y2){
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
 
+	void lineComparison() {
+		float line = (float)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Length of first Line : " + line);
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Line Comparison Problem");
+		LineComparison lc = new LineComparison(5, 10, 20, 15);
+		lc.lineComparison();
+	
 	}
 
 }
